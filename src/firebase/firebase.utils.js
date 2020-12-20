445fb323997bd0491c92b1d5f5777db3e9672e43
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2020-12-20 20:21:07 +0100

    storing user data in firebase & app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore"; //database
 import "firebase/auth";
 
+// Basic Config
 const config = {
   apiKey: "AIzaSyCAfLcL6x9ZHmMXIHKQ9qn28N86wY3fris",
   authDomain: "crwn-shop-db-3c3c3.firebaseapp.com",
@@ -9,6 +10,34 @@ const config = {
   storageBucket: "crwn-shop-db-3c3c3.appspot.com",
   messagingSenderId: "150696341373",
   appId: "1:150696341373:web:b6c9857b4fdf78887d6614",
+};
+
+// Store authenticated users into db
+export const createUserProfilDocument = async (userAuth, additionalData) => {
+  // If signin out, no need to do anything, exit the func
+  if (!userAuth) return;
+
+  // Query firestore to see if user already exists & create a new user in the db if it dosnt
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
